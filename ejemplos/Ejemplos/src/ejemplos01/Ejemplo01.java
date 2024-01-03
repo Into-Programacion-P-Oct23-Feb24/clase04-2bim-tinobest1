1c1efe215d143c039251f6217e569d7ffb8c6d8f
TinoBest <sayayinlenin@hotmail.com>
2024-01-03 16:57:06 -0500

    -atrayh

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -16,25 +16,33 @@ public class Ejemplo01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        String miCiudad = obtenerCiudadMayuscula("Loja");
-        System.out.printf("%s\n", miCiudad);
+
+        //String miCiudad = obtenerCiudadMayuscula("Loja");
+        //System.out.printf("%s\n", miCiudad);
+        obtenerMultiplicacion(7, 50);
+        //E
+
+        String Mensaje = obtenerMultiplicacion(7, 50);
+        System.out.printf("%s\n", Mensaje);
+
+   // public static String obtenerCiudadMayuscula(String m) {
+   //Aqui el error nos daba que no teniamos el static ya que no tenia que volver
+       // String m2 = m.toUpperCase();
+       // return m2;
     }
-    
-    
-    public String obtenerCiudadMayuscula(String m){
-        String m2 = m.toUpperCase();
-        return m2;
+
+    public static String obtenerMultiplicacion(int tabla, int limite) {
+
+        int operacion;
+        String cadena = "";
+        for (int i = 1; i <= limite; i++) {
+            operacion = tabla * i;
+            cadena = String.format("%s%d * %d = %d\n", cadena,
+                    tabla, i,
+                    operacion);
+        }
+
+        return cadena;
     }
-    
-    public static void obtenerMultiplicacion(int tabla, int limite){
-        
-    }
-    
-    public static String obtenerNombre(){
-        return "Luis";
-    }
-    
-    
+
 }
