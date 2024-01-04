e459ac3076825c30ac1dcbf298a4075353747f9f
TinoBest <sayayinlenin@hotmail.com>
2024-01-04 15:26:53 -0500

    -atrabajo

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -14,26 +14,26 @@ public class Ejemplo041 {
     public static void main(String[] args) {
         // 
         String cadenaFinal = "";
-        int[][] arreglo1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
-        int[][] arreglo2 = {{1, 2, 3}, {2, 2, 2}, {3, 2, 1}};
-        int[][] arreglo3 = new int[3][3];
+        int[][] one  = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] tho = {{1, 2, 3}, {2, 2, 2}, {3, 2, 1}};
+        int[][] tree = new int[3][3];
 
-        for (int i = 0; i < arreglo1.length; i++) {
-            for (int j = 0; j < arreglo1[i].length; j++) {
-                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j],
-                        arreglo2[i][j]);
+        for (int i = 0; i < one.length; i++) {
+            for (int j = 0; j < tho[i].length; j++) {
+                tree[i][j] = obtenerMultiplicacion(one[i][j],
+                        tho[i][j]);
             }
         }
-        for (int i = 0; i < arreglo1.length; i++) {
-            for (int j = 0; j < arreglo1[i].length; j++) {
-                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j],
-                        arreglo2[i][j]);
+        for (int i = 0; i < one.length; i++) {
+            for (int j = 0; j < one[i].length; j++) {
+                tree[i][j] = obtenerMultiplicacion(one[i][j],
+                        tho[i][j]);
             }
         }
-        for (int i = 0; i < arreglo1.length; i++) {
-            for (int j = 0; j < arreglo1[i].length; j++) {
+        for (int i = 0; i < tho.length; i++) {
+            for (int j = 0; j < one[i].length; j++) {
                 System.out.printf(" %s %d", cadenaFinal,
-                        arreglo3[i][j]);
+                        tree[i][j]);
                 if (j == 2) {
                     System.out.print("\n");
                 }
