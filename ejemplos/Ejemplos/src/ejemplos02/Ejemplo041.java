db1e60a7675ff862f00967b6615e9d918ea1cb9f
TinoBest <sayayinlenin@hotmail.com>
2024-01-04 15:21:40 -0500

    -atrumble

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -13,16 +13,39 @@ public class Ejemplo041 {
 
     public static void main(String[] args) {
         // 
-        obtenerMultiplicacion(primerValor, segundoValor); 
-        
+        String cadenaFinal = "";
+        int[][] arreglo1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] arreglo2 = {{1, 2, 3}, {2, 2, 2}, {3, 2, 1}};
+        int[][] arreglo3 = new int[3][3];
+
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j],
+                        arreglo2[i][j]);
+            }
+        }
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j],
+                        arreglo2[i][j]);
+            }
+        }
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                System.out.printf(" %s %d", cadenaFinal,
+                        arreglo3[i][j]);
+                if (j == 2) {
+                    System.out.print("\n");
+                }
+            }
+        }
+
     }
-        
-    public static void obtenerSuma(int a, int b){
-        int suma;
-        suma = a + b;
-        System.out.printf("El valor de la suma es: %d\n", suma);
-        
+
+    public static int obtenerMultiplicacion(int a, int b) {
+        int multiplicacion;
+        multiplicacion = a * b;
+        return multiplicacion;
     }
-    
-    
+
 }
